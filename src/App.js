2efab9dc82c09d87b61e12700c8deb7ec0b07c61
Montharop T <jaava@users.noreply.github.com>
2024-11-21 17:33:31 +0700

    sec2.9

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import GroupList from './components/group-list';
 
 function App() {
-  const [groups, setGroups ] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setLoading(true)
-    const getData = async () => {
-      await fetch('http://127.0.0.1:8888/api/groups/')
-      .then(response => response.json())
-      .then(data => {
-        setGroups(data)
-        setLoading(false)})
-      .catch(error => {
-        setError(true)
-        setLoading(false)
-      })
-    }
-    getData();
-  },[])
-
-  if(error) {
-    return <h1>Error</h1>
-  }
-
-  if(loading) {
-    return <h1>Loading...</h1>
-  }
-
+ 
   return (
     <div className="App">
       <header className="App-header">
       
-        {groups && groups.map(group => {
-          return <p key={group.id}>{group.name}: {group.location}</p>
-        })}
+        <GroupList />
         
       </header>
     </div>
---
+++ src/components/group-list.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+function GroupList() {
+    const [groups, setGroups] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true)
+        const getData = async () => {
+            await fetch('http://127.0.0.1:8888/api/groups/')
+                .then(response => response.json())
+                .then(data => {
+                    setGroups(data)
+                    setLoading(false)
+                })
+                .catch(error => {
+                    setError(true)
+                    setLoading(false)
+                })
+        }
+        getData();
+    }, [])
+
+    if (error) {
+        return <h1>Error</h1>
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            {groups && groups.map(group => {
+                return <p key={group.id}>{group.name}: {group.location}</p>
+            })}
+
+        </div>
+    );
+}
+
+export default GroupList;
