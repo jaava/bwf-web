448f1fd7b96fdf4b8e9e3a4079c85670f22b441d
Montharop T <jaava@users.noreply.github.com>
2024-11-25 16:36:37 +0700

    sec3.11

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import GroupList from './components/group-list';
+import Header from './components/header';
+import Main from './components/main';
+import Sidebar from './components/sidebar';
 
 function App() {
  
   return (
     <div className="App">
       <header className="App-header">
-      
-        <GroupList />
-        
+        <Header />
+        <div>
+          <Sidebar />
+          <Main />
+          
+        </div>
       </header>
     </div>
   );
