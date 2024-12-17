246c640a894e7a132130e684023c1b779afa8a0e
Montharop T <jaava@users.noreply.github.com>
2024-12-17 16:47:21 +0700

    sec4.23

--- src/components/group-detail.js
+++ src/components/group-detail.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import useFetchGroup from '../hooks/fetch-group';
 
 function GroupDetail() {
@@ -24,7 +24,16 @@ function GroupDetail() {
     return (
         <div>
             <Link to={'/'}>Back</Link>
-            {group && <h1>Group Detail {group.id}{group.name}: {group.location}</h1>}
+            {group && <Fragment>
+                <h1>Group Detail {group.id}{group.name}: {group.location}</h1>
+                <h2>{group.description}</h2>
+                <h3>Events:</h3>
+                { group.events.map(event=> {
+                    return <div key={event.id}>
+                        <p>{event.team1} VS {event.team2}</p>
+                        </div>
+                })}
+            </Fragment>}
             
         </div>
     );
