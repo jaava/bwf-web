fd8de60d24910ef8f89c4b1723c9695b61922efa
Montharop T <jaava@users.noreply.github.com>
2025-01-20 17:26:19 +0700

    sec6.66

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';  
 import { makeStyles } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
 import useFetchEvent from '../../hooks/fetch-event';
 import { useAuth } from '../../hooks/useAuth';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import { DateTime } from 'luxon';
 
 const useStyles = makeStyles(theme => ({
 
@@ -15,10 +19,40 @@ export default function Event() {
     const classes = useStyles();
     const [ data, loading, error ] = useFetchEvent(authData.token,id);
     const [ event, setEvent ] = useState(null);
+    const [ evtTime , setEvtTime ] = useState(null);
+
+    useEffect(() => {
+
+        setEvent(data);
+        if(data?.time){
+            const format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+            
+            setEvtTime(DateTime.fromFormat(data.time, format));
+        }
+    }, [data])
+
+
+    if (error) {
+        return <h1>Error</h1>
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+
+    
+
     return (
         <Fragment>
-            <h3>Events info here</h3>
-            
+            { event && evtTime && <div>
+                <h3>{event.team1} VS {event.team2}</h3>
+            <h2>
+            <CalendarTodayIcon className={classes.dateTime}/>{evtTime.toSQLDate()} 
+            <AlarmIcon className={classes.dateTime}/>{evtTime.toFormat('HH:mm')}
+            </h2>
+            </div>
+            }
         </Fragment>
     );
 }
