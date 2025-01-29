ffe0a62ae1b1f3f49074aee97c83f272fa026365
Montharop T <jaava@users.noreply.github.com>
2025-01-29 16:38:20 +0700

    sec6.74

--- src/components/events/event-form.js
+++ src/components/events/event-form.js
@@ -1,14 +1,46 @@
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { CssTextField } from '../layout/elements';
+import { Button } from '@material-ui/core';
 
 export default function EventForm() {
 
     const { state } = useLocation();
     const { group } = state;
+    const [team1, setTeam1] = useState();
+    const [team2, setTeam2] = useState();
+    const [time, setTime] = useState();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('submitted', team1, team2, time);
+    };
     return (
         <div>
             <h1>New event for a group {group.id}</h1>
-            <CssTextField   label="Team 1"/>
+            <form onSubmit={handleSubmit}>
+                <CssTextField label="Team 1" onChange={e => setTeam1(e.target.value)} />
+                <CssTextField label="Team 2" onChange={
+                    e => setTeam2(e.target.value)
+                } />
+                <br />
+                <CssTextField
+                    id="datetime-local"
+                    label="Date and Time of Event"
+                    type="datetime-local"
+
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+
+                    onChange={e => setTime(e.target.value)}
+                />
+                <br />
+                <Button type="submit" variant="contained" color="primary">
+                    Create Event
+                </Button>
+            </form>
+
         </div>
     );
 }
