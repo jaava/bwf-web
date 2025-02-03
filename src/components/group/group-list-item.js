008995142687aaf3dbbd6e2e06ba0aff60e5b046
Montharop T <jaava@users.noreply.github.com>
2025-02-03 17:36:36 +0700

    sec8.88

--- src/components/group/group-list-item.js
+++ src/components/group/group-list-item.js
@@ -1,27 +1,21 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
-
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import  PeopleIcon from '@material-ui/icons/People'
 const useStyles = makeStyles(theme => ({
-    dateTime: {
-        fontSize: '18px',
-        marginRight: '3px',
-        marginTop: '10px',
-        color: theme.colors.mainAccentColor
-    },
-    memberContainer:{
+    container:{
+        cursor: 'pointer',
+        textAlign: 'left',
+        border: '2px solid #fff',
+        borderRadius: '1rem',
+        padding: '0 1rem',
         display: 'grid',
-        gridTemplateColumns: 'auto 5fr 1fr',
-        alignItems: 'center',
+        gridTemplateColumns: '5fr 1fr',
+        marginBottom: '1rem'
     },
-    gold: {
-        color: 'gold'
-    },
-    silver: {
-        color: 'silver'
-    },
-    bronze: {
-        color: 'brown'
+    name:{
+        color: theme.palette.primary.main
     }
 }));
 
@@ -30,8 +24,12 @@ function GroupListItem({group, onClicked}) {
 
     return (
         <div>
-            {group && <div key={group.id} onClick={()=>onClicked(group.id)}>
-                        <p>{group.name}: {group.location}</p>
+            {group && <div key={group.id} onClick={()=>onClicked(group.id)} className={classes.container}>
+                        <h3><span className={classes.name}>{group.name}</span>: <LocationOnIcon/>{group.location}</h3>
+                        <h3>
+                            <PeopleIcon />{group.num_members}
+                        </h3>
+                        <p>{group.description}</p>
                     </div>
             }
 
